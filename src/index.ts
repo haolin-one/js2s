b340fdccad145c965de32fd9e7bbc0814225f19a
wanghaolin <@wanghaolin>
2025-07-30 14:59:16 +0800

    build: 初始化项目结构和配置
    - 新增 .gitignore 和 .npmignore 文件，配置忽略项
    - 创建 package.json，设置项目基本信息
    - 添加 src 目录，实现字符串处理功能
    - 新增 tsconfig.json，配置 TypeScript 编译选项

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/*
+ * @Date: 2025-07-30 14:50:28
+ * @LastEditors: wanghaolin @wanghaolin
+ * @LastEditTime: 2025-07-30 14:52:14
+ */
+export * from "./string";
---
+++ src/string.ts
@@ -0,0 +1,8 @@
+/*
+ * @Date: 2025-07-30 14:50:39
+ * @LastEditors: wanghaolin @wanghaolin
+ * @LastEditTime: 2025-07-30 14:50:57
+ */
+export function capitalize(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
